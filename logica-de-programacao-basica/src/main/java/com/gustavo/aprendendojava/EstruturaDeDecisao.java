9befde169c5fb3f2f30a24ff13f71393b8f86ace
Gustavo Lettiere <86628913+gustavolettiere@users.noreply.github.com>
2023-10-20 10:13:40 -0300

    Initial commit

---
+++ logica-de-programacao-basica/src/main/java/com/gustavo/aprendendojava/EstruturaDeDecisao.java
@@ -0,0 +1,11 @@
+package com.gustavo.aprendendojava;
+
+public class EstruturaDeDecisao {
+    public static void main(String[] args) {
+        if (false){
+            System.out.println("Verdadeiro");
+        }else{
+            System.out.println("Falso");
+        }
+    }
+}
